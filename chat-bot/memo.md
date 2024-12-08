ecdbbee1e0b437cffa743f7061fbd297afbc753f
Sadao Hiratsuka <sh2@pop01.odn.ne.jp>
2024-12-08 15:04:27 +0900

    chore: Update README, Dockerfile, and requirements; add memo for package list

---
+++ chat-bot/memo.md
@@ -0,0 +1,6 @@
+# Package List
+
+- httpx[socks]<0.28
+- mmpy-bot<2.2.0
+- openai
+- websocket-client
--- chat-bot/openai-chat-bot.py
+++ chat-bot/openai-chat-bot.py
@@ -40,7 +40,7 @@ class ChatBot(Plugin):
         openai_service = os.environ.get("AZURE_OPENAI_SERVICE", "")
 
         if openai_proxy:
-            http_client = httpx.Client(proxies=openai_proxy)
+            http_client = httpx.Client(proxy=openai_proxy)
 
         if openai_service:
             # If the environment variable AZURE_OPENAI_SERVICE is defined, use Azure OpenAI.
